3fbd9b2985d407eaf9f72e18eebcd37bd794bc1e
n4mlz <n4mlz.dev@gmail.com>
2025-07-30 19:53:50 +0900

    :zap: Enhance parser to support additional nodes

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,6 @@
 use crate::lexer::Token;
+use std::iter::Peekable;
+use std::slice::Iter;
 
 #[derive(Debug, Clone)]
 pub enum Node {
@@ -9,12 +11,34 @@ pub enum Node {
     Output,
     Input,
     Loop(Vec<Node>),
+    Parallel(Vec<Vec<Node>>),
+    LockAcquire,
+    LockRelease,
+    Wait(usize),
 }
 
-fn parse_inner(iter: &mut dyn Iterator<Item = &Token>) -> Vec<Node> {
+fn parse_parallel(iter: &mut Peekable<Iter<Token>>) -> Vec<Vec<Node>> {
+    let mut branches = Vec::new();
+    loop {
+        let branch = parse_nodes(iter, &[Token::ParSep, Token::ParEnd]);
+        branches.push(branch);
+        match iter.next() {
+            Some(Token::ParSep) => continue,
+            Some(Token::ParEnd) => break,
+            other => panic!("Expected '|' or '}}', found {other:?}"),
+        }
+    }
+    branches
+}
+
+fn parse_nodes(iter: &mut Peekable<Iter<Token>>, terminators: &[Token]) -> Vec<Node> {
     let mut nodes = Vec::new();
-    while let Some(token) = iter.next() {
-        match token {
+    while let Some(&token_ref) = iter.peek() {
+        let token = token_ref;
+        if terminators.contains(token) {
+            break;
+        }
+        match iter.next().unwrap() {
             Token::IncPtr => nodes.push(Node::IncPtr),
             Token::DecPtr => nodes.push(Node::DecPtr),
             Token::IncCell => nodes.push(Node::IncCell),
@@ -22,18 +46,31 @@ fn parse_inner(iter: &mut dyn Iterator<Item = &Token>) -> Vec<Node> {
             Token::Output => nodes.push(Node::Output),
             Token::Input => nodes.push(Node::Input),
             Token::LoopStart => {
-                let body = parse_inner(iter);
+                let body = parse_nodes(iter, &[Token::LoopEnd]);
+                iter.next();
                 nodes.push(Node::Loop(body));
             }
-            Token::LoopEnd => {
-                break;
+            Token::ParStart => {
+                let branches = parse_parallel(iter);
+                nodes.push(Node::Parallel(branches));
             }
+            Token::LockStart => nodes.push(Node::LockAcquire),
+            Token::LockEnd => nodes.push(Node::LockRelease),
+            Token::Wait => {
+                let mut count = 1;
+                while let Some(&&Token::Wait) = iter.peek() {
+                    count += 1;
+                    iter.next();
+                }
+                nodes.push(Node::Wait(count));
+            }
+            _ => break,
         }
     }
     nodes
 }
 
 pub fn parse(tokens: &[Token]) -> Vec<Node> {
-    let mut iter = tokens.iter();
-    parse_inner(&mut iter)
+    let mut iter = tokens.iter().peekable();
+    parse_nodes(&mut iter, &[])
 }
