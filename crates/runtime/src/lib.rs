4c8c5b9bc1ab03a77961848fe49d29b2e40f4bee
n4mlz <n4mlz.dev@gmail.com>
2025-08-12 19:22:06 +0900

    :sparkles: Define State struct

--- crates/runtime/src/lib.rs
+++ crates/runtime/src/lib.rs
@@ -1,4 +1,20 @@
+#[repr(C)]
+#[derive(Debug)]
+pub struct State {
+    tape_base: *mut i8,
+    ptr_index: i64,
+    mutex_slab: *mut i8,
+    lock_stack: *mut i64,
+    lock_sp: i64,
+    lock_cap: i64,
+}
+
 #[unsafe(no_mangle)]
 pub extern "C" fn hello() {
     println!("Hello, world!");
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn print_state(state: &State) {
+    println!("State:{state:?}");
 }
